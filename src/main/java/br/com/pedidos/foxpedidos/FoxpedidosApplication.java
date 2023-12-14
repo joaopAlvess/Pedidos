e0ed3a93658e0f85d9041f72608b3f40c10c5bc6
joaopAlvess <jpfox.alves@gmail.com>
2023-12-14 13:25:04 -0300

    first commit

---
+++ src/main/java/br/com/pedidos/foxpedidos/FoxpedidosApplication.java
@@ -0,0 +1,13 @@
+package br.com.pedidos.foxpedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoxpedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoxpedidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pedidos/foxpedidos/FoxpedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pedidos.foxpedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoxpedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
