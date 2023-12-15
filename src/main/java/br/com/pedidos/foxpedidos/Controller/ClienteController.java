6d6628b315195f5173a7fb3a4be886c27d5f6ebf
joaoPedro <jpfox.alves@gmail.com>
2023-12-15 11:12:07 -0300

    Repository e JPA do cliente criados

--- src/main/java/br/com/pedidos/foxpedidos/Controller/ClienteController.java
+++ src/main/java/br/com/pedidos/foxpedidos/Controller/ClienteController.java
@@ -1,17 +1,24 @@
 package br.com.pedidos.foxpedidos.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.pedidos.foxpedidos.Domain.Cliente.ClienteRepository;
 import br.com.pedidos.foxpedidos.Domain.Cliente.DTOCadastroCliente;
 
 @RestController
 @RequestMapping("/cliente")
 public class ClienteController {
     
-    public ResponseEntity cadastrar(@RequestBody DTOCadastroCliente data){
-        return ResponseEntity.ok(data);
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @PostMapping
+    public void cadastrar(@RequestBody String data){
+        System.out.println(data);
     }
 }
--- src/main/java/br/com/pedidos/foxpedidos/Domain/Cliente/Cliente.java
+++ src/main/java/br/com/pedidos/foxpedidos/Domain/Cliente/Cliente.java
@@ -1,8 +1,30 @@
 package br.com.pedidos.foxpedidos.Domain.Cliente;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Table(name = "clientes")
+@Entity(name = "Cliente")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
 public class Cliente {
 
-    
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String endereco;
+    private String telefone;
+    private String valorAdicional;
     
 }
