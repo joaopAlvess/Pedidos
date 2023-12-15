e2405717db3053950e879761c8c7255a667fd373
joaoPedro <jpfox.alves@gmail.com>
2023-12-15 15:00:43 -0300

    Requisição GET de clientes

--- src/main/java/br/com/pedidos/foxpedidos/Controller/ClienteController.java
+++ src/main/java/br/com/pedidos/foxpedidos/Controller/ClienteController.java
@@ -1,7 +1,11 @@
 package br.com.pedidos.foxpedidos.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,5 +29,11 @@ public class ClienteController {
     public ResponseEntity<Void> cadastrar(@RequestBody @Valid DTOCadastroCliente data){
         clienteRepository.save(new Cliente(data));
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping    
+    public ResponseEntity<Page<Cliente>> listar(@PageableDefault(size = 10, sort = {"nome"})Pageable paginacao){
+        Page<Cliente> clientes = clienteRepository.findAll(paginacao);
+        return ResponseEntity.ok(clientes);
     }
 }
